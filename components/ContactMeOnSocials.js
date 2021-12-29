448bcf17da8825c54f97710ba3414bc0af7a1340
Oussema <Oussema.dhraief.17@gmail.com>
2021-12-29 02:06:48 +0000

    c

--- components/ContactMeOnSocials.js
+++ components/ContactMeOnSocials.js
@@ -12,7 +12,7 @@ export default function ContactMeOnSocials(){
     return( 
         <div id="ContactMeImage" className="w-11/12 p-5 md:p-0 md:w-4/6 h-96 md:min-h-full relative space-y-11">
                 <h1 className="text-center text-xl font-bold font-mono mt-5 text-gray-900">Get in touch with me or leave me a review !</h1>
-                <span className="absolute h-fit w-fit bottom-0 left-0 bg-transparent"><Image src={robot} alt="Robot Gif" width={250} height={250} className="bg-transparent" /></span>
+                <span className="absolute h-fit w-fit bottom-0 z-10 left-0 bg-transparent"><Image src={robot} alt="Robot Gif" width={250} height={250} className="bg-transparent" /></span>
                 <div className="flex space-x-3 w-fit mx-auto content-center my-auto">
                 <SocialMediaLink smLogo={fb} hoverText="Facebook" altText="Facebook Icon" smLink="https://www.facebook.com/oussema.dhraief/" />
                 <SocialMediaLink smLogo={insta} hoverText="Instagram" altText="Instagram Icon" smLink="https://www.instagram.com/oussema.dhraief/" />

e4ccf339dd0da52d8b5e128810930aeb0015ce27
Oussema <Oussema.dhraief.17@gmail.com>
2021-12-29 13:35:48 +0000

    commit

--- components/EducationSection.js
+++ components/EducationSection.js
@@ -2,7 +2,7 @@ import EducationElement from './EducationElement'
 
 export default function EducationSection(){
     return (
-        <div id="EducationPart" className='h-72 space-y-8 sm:space-y-24 py-5 sm:py-10'>  
+        <div id="EducationPart" className='h-72 space-y-8 sm:space-y-24 py-5 2xl:py-10'>  
             <EducationElement year="2019-2021" institute="Higher Institute of Applied Science and Technology of Sousse" degree="Bachelor's degree of Computer Science" />
             <EducationElement year="2015-2019" institute="Hammem Sousse II High School" degree="High school diploma"/>
         </div>

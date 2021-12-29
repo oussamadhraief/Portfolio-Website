1fe9c86b5dd75c4c0ebd662323a55b37b2c91266
Oussema <Oussema.dhraief.17@gmail.com>
2021-12-29 00:34:37 +0000

    commit

--- components/Intro.js
+++ components/Intro.js
@@ -3,7 +3,7 @@ import Description from './Description'
 
 export default function Intro({ championData }){
 
-    console.log(props.championData);
+    console.log(championData);
 
     return (
             <div className="mx-auto drop-shadow-2xl mt-10 pb-3 lg:pb-0 mb-5 h-fit w-11/12 xl:w-5/6 2xl:w-9/12 3xl:w-4/6 grid lg:flex shadow-3xl">

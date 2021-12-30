e6f6ea591943b4df014c41cfc24d448e3e03a5eb
Oussema <Oussema.dhraief.17@gmail.com>
2021-12-30 14:56:41 +0000

    commit

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -2,9 +2,10 @@ import ProjectComponent from './ProjectComponent'
 
 export default function ProjectsSection(){
     return (
-        <div id="ProjectsPart" className='h-72 mt-5 space-y-3'>
-            <ProjectComponent linkToProject="https://github.com/oussamadhraief/Oussama-Dhraief-Portfolio" projectTitle="Portfolio-Website" projectDescription="This is my portfolio website." stars="17" forks="8" />
-            <ProjectComponent linkToProject="https://github.com/oussamadhraief/ClaigsRist-Project" projectTitle="ClaigsRist-Project" projectDescription="This is a personal project - E-commerce website." stars="13" forks="2" />
+        <div id="ProjectsPart" className='h-72 mt-5 2xl:pl-5 space-y-3 pb-14 overflow-y-scroll'>
+            <ProjectComponent linkToProject="https://github.com/oussamadhraief/Oussama-Dhraief-Portfolio" projectTitle="Portfolio-Website" projectDescription="My portfolio website created with Next.js (ReactJS), Tailwind CSS, Node.js and MongoDB." stars="17" forks="8" />
+            <ProjectComponent linkToProject="https://github.com/oussamadhraief/ClaigsRist-Project" projectTitle="ClaigsRist-Project" projectDescription="This is an E-commerce website created using HTML, CSS, JavaScript and Firebase. " stars="13" forks="2" />
+         
         </div>
     )
 }

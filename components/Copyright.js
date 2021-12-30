6cfc6d7c512c73990c0307e9c1afaafc03f9cdd6
Oussema <Oussema.dhraief.17@gmail.com>
2021-12-30 13:37:33 +0000

    commit

--- components/Copyright.js
+++ components/Copyright.js
@@ -3,11 +3,11 @@ import FooterContainer from './FooterContainer'
 
 export default function Copyright(){
 
-    const bgImage = "download2_qlvdfr.png"
+    const bgImage = "download2_bfdqxg.svg"
     
     return(
         <footer className="w-full bg-gray-900 mt-10 h-fit mb-8">
-            <div className="bg-gray-900   w-screen relative">
+            <div className="w-screen relative bg-white">
             <Image src={bgImage} alt="Wavy background white image" width={300} height={20} quality={100} layout='responsive'/>
             </div>
             <FooterContainer />

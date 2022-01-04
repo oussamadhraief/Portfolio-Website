f1483f9e9658cab99e30aca83af84357e3298636
Oussema <Oussema.dhraief.17@gmail.com>
2022-01-04 12:48:30 +0000

    commit

--- components/Intro.js
+++ components/Intro.js
@@ -6,9 +6,16 @@ export default function Intro({ championData }){
     const serverSide = championData
 
     return (
-            <div className="mx-auto drop-shadow-2xl mt-10 pb-3 lg:pb-0 mb-5 h-fit w-11/12 xl:w-5/6 2xl:w-9/12 3xl:w-4/6 grid lg:flex shadow-3xl">
+            <div className="mx-auto grid mt-10 pb-3 rounded-lg lg:pb-0 mb-5 h-fit w-11/12 xl:w-5/6 2xl:w-9/12 3xl:w-4/6 border-[1px] shadow-lg">
+                <div className="h-10 flex bg-neutral-100 space-x-2  items-center rounded-t-lg">
+                    <span className="w-4 h-4 bg-red-400 rounded-full ml-5 shadow-2xl hover:cursor-pointer hover:scale-105"></span>
+                    <span className="w-4 h-4 bg-yellow-500 rounded-full shadow-2xl hover:cursor-pointer hover:scale-105"></span>
+                    <span className="w-4 h-4 bg-green-500 rounded-full shadow-2xl hover:cursor-pointer hover:scale-105"></span>
+                </div>
+                <div className="grid lg:flex w-full relative h-fit">
                 <SideImage />
                 <Description />
+                </div>
             </div>
     )
 }
--- components/SideImage.js
+++ components/SideImage.js
@@ -5,8 +5,8 @@ export default function SideImage(){
     const coding = "coding_tfbugk.gif"
     
     return ( 
-        <div className="w-full h-fit lg:w-2/3 2xl:w-8/12 bg-gray-900">
-            <Image src={coding} alt="Me Coding GIF - Web development" layout="responsive" width={600} height={600} className="bg-gray-900" objectPosition={0} quality={100} loading="lazy" />
+        <div className="w-full h-fit lg:w-2/3 2xl:w-8/12 bg-gray-900 rounded-bl-lg">
+            <Image src={coding} alt="Me Coding GIF - Web development" layout="responsive" width={600} height={600} className="bg-gray-900  rounded-bl-lg" objectPosition={0} quality={100} loading="lazy" />
         </div>
     )
 }

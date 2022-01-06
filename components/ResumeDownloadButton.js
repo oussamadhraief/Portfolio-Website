43df878b7ef638ad0e1003624af3233dde89563a
Oussema <Oussema.dhraief.17@gmail.com>
2022-01-06 22:57:02 +0000

    commit

--- components/ResumeDownloadButton.js
+++ components/ResumeDownloadButton.js
@@ -2,6 +2,6 @@ import Link from "next/link"
 
 export default function ResumeDownloadButton(props){
     return(
-        <Link href="/Oussema-Dhraief-CV.pdf"><a target="_blank" className={props.styling}>Get Resume</a></Link>
+        <Link href="/Oussema-Dhraief-CV-English.pdf"><a target="_blank" className={props.styling}>Get Resume</a></Link>
     )
 }

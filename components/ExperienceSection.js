9afd39967f7ce1973efafe0c979763f2cecf3157
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-02 16:28:41 +0000

    commit

--- components/ExperienceSection.js
+++ components/ExperienceSection.js
@@ -5,7 +5,7 @@ export default function ExperienceSection(){
         <div id="ExperiencePart" className='h-72 pl-3 overflow-y-scroll w-fit 2xl:w-94 pt-2 2xl:pt-5'>
             <h1 className="text-gray-900 font-semibold 2xl:font-bold">End of Studies Internship</h1>
             <p className="font-thin sm:font-normal">
-            Developing and maintaining an E-commerce web application at Medical and surgical suppliers (Full-Stack Developer).
+            Developing and maintaining an E-commerce web application at QUICK Medical services (Full-Stack Developer).
             </p>
             <h1 className="text-gray-900 font-medium 2xl:font-semibold">Technologies used:</h1>
             <Skills skillText="Next.js (ReactJS)" />

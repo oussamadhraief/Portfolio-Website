6a86c1ca4b5c3c8156e774a08ed582aacebaaded
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-02-18 14:06:34 +0100

    commit

--- components/SideImage.js
+++ components/SideImage.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import { useEffect, useState,useContext } from "react"
 import Monster from "./Monster"
-import noSsr from "./no-ssr"
+import NoSsr from "./no-ssr"
 import { LoadingContext } from "../utils/LoadingContext";
 
 
@@ -38,9 +38,9 @@ export default function SideImage(){
         <div className="h-fit w-full lg:w-96 bg-gray-900 md:rounded-bl-lg relative sm:grid">
             {phone ? null : <p className="text-white mx-auto mt-1">Hey, play with me</p>}
             {phone ? <Image src={coding} alt="Me Coding GIF - Web development" layout="responsive" width={600} height={600} className="bg-gray-900  rounded-bl-lg" objectPosition={0} quality={100} loading="lazy" /> :
-            <noSsr>
+            <NoSsr>
                 <Monster />
-            </noSsr>}
+            </NoSsr>}
         </div>
     )
 }

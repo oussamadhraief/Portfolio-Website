e53fa0059dfe6a6e5745f7d40b3742d5712e88eb
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-02-19 02:14:56 +0100

    commit

--- components/IdentityImage.js
+++ components/IdentityImage.js
@@ -2,10 +2,10 @@ import Image from "next/image"
 
 export default function MainSection(){
 
-    const mainPic = "mainPic_m8jjqm.png"
+    const mainPic = "mainPic_m8jjqm_hg4jcc.png"
 
     return (
-            <div className="mx-auto sm:w-1/2 sm:mx-0 3xl:w-1/4 pt-6 2xl:w-1/3 xl:w-1/3 w-full h-auto relative">
+            <div className="mx-auto sm:w-1/2 sm:mx-0 3xl:w-1/4 pt-8 2xl:w-1/3 xl:w-1/3 w-full h-auto relative">
                 
                 <Image src={mainPic} alt="that's me picture" quality={100} width={300} height={300} layout="responsive" priority />
             </div>

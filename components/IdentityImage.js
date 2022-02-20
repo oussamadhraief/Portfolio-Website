87812c4d6e1bb8921edaf21b98788a9068648635
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-02-20 12:17:39 +0100

    commit

--- components/IdentityImage.js
+++ components/IdentityImage.js
@@ -5,7 +5,7 @@ export default function MainSection(){
     const mainPic = "mainPic_m8jjqm_hg4jcc.png"
 
     return (
-            <div className="mx-auto sm:w-1/2 sm:mx-0 3xl:w-1/4 pt-8 2xl:w-1/3 xl:w-1/3 w-full h-auto relative">
+            <div className="mx-auto sm:w-1/2 sm:mx-0 3xl:w-1/4 pt-8 2xl:w-1/3 xl:w-1/3 w-full h-auto relative hover:scale-95 transition-all hover:cursor-pointer">
                 
                 <Image src={mainPic} alt="that's me picture" quality={100} width={300} height={300} layout="responsive" priority />
             </div>

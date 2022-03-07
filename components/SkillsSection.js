6f3afd62d9f3f51e6fd455897284051d3aa1cf07
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-03-07 11:57:21 +0100

    commit

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -15,22 +15,23 @@ export default function SkillsSection(){
     const firebase = "firebase_ztz5rt.png"
     const git = "git_b23wnr.svg"
     const tailwind = "tailwind_k1zaze.png"
+    const threejs = "Three.js_Icon.svg_i13nrx.png"
 
     return ( 
-        <div id="SkillsPart" className='h-72 overflow-y-scroll overflow-x-hidden flex flex-wrap space-x-3 sm:space-x-5 pt-3 md:py-3 justify-center'>
+        <div id="SkillsPart" className='h-72 overflow-y-scroll overflow-x-hidden flex flex-wrap gap-3 md:gap-3 pt-3 px-5 md:py-5 justify-center'>
             <WebSkill srcPath={html} altText="HTML Icon" hoverText="HTML" />
             <WebSkill srcPath={css} altText="CSS Icon" hoverText="CSS" />
             <WebSkill srcPath={js} altText="Javascript Icon" hoverText="JavaScript" />
             <WebSkill srcPath={react} altText="ReactJS Icon" hoverText="ReactJS" />
             <WebSkill srcPath={next} altText="Next.js Icon" hoverText="Next.js" />
             <WebSkill srcPath={node} altText="Node.js Icon" hoverText="Node.js" />
-            <br></br>
             <WebSkill srcPath={express} altText="Express.js Framework Icon" hoverText="Express.js" />
             <WebSkill srcPath={mongo} altText="MongoDB Mongoose Icon" hoverText="MongoDB" />
             <WebSkill srcPath={ts} altText="TypeScript Icon" hoverText="TypeScript" />
             <WebSkill srcPath={firebase} altText="Firebase Icon" hoverText="Firebase" />
             <WebSkill srcPath={tailwind} altText="Tailwind Icon" hoverText="Tailwind" />
             <WebSkill srcPath={git} altText="Git Icon" hoverText="Git" />
+            <WebSkill srcPath={threejs} altText="Three.js Icon" hoverText="Three.js" />
         </div>
     )
 }

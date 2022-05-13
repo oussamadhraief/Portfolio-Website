f0d859a5fe627d44526d83dca4e0f4831142858b
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-05-13 16:22:18 +0100

    c

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -9,7 +9,7 @@ async function dbConnect(){
         return
     }
 
-    const db = await mongoose.connect(process.env.MONGO_URI, {
+    const db = mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })

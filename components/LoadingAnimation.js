603143afadc3d55fb3619ccfee14b7d11d86c480
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-05-13 15:15:36 +0100

    commit

--- components/LoadingAnimation.js
+++ components/LoadingAnimation.js
@@ -2,12 +2,10 @@
 export default function LoadingAnimation(){
 
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden bg-white z-[9999] grid content-center">
-                <div className="loadingAnimation flex justify-center items-center flex-nowrap mb-28">
-                    <span className="w-2 h-2 bg-zinc-600 rounded-full mx-2"></span>
-                    <span className="w-2 h-2 bg-slate-900 rounded-full mx-1"></span>
-                    <span className="w-2 h-2 bg-secondary rounded-full mx-2"></span>
-                </div>
-        </div>
+        <div className='bg-white h-screen w-screen overflow-hidden flex items-center absolute z-[9999] left-0 top-0'>
+      <div id="contact-loading" className="w-fit h-fit bg-white/70 z-[9999] mx-auto ">
+        <div className="reverse-spinner "></div>
+      </div>
+    </div>
     )
 }

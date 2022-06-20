e790c2844b791108a0162fdda89fb4c5c4efd57e
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-06-20 19:15:51 +0100

    commit

--- components/LoadingAnimation.js
+++ components/LoadingAnimation.js
@@ -2,8 +2,8 @@
 export default function LoadingAnimation(){
 
     return (
-        <div className='bg-white h-full w-full overflow-hidden flex items-center absolute z-[9999] left-0 top-0'>
-      <div id="contact-loading" className="w-fit h-fit bg-white/70 z-[9999] mx-auto ">
+        <div className='bg-white inset-0 overflow-hidden flex items-center absolute '>
+      <div id="contact-loading" className="w-fit h-fit bg-white/70 mx-auto ">
         <div className="reverse-spinner "></div>
       </div>
     </div>
--- components/SideImage.js
+++ components/SideImage.js
@@ -42,7 +42,7 @@ export default function SideImage(){
             <NoSsr>
                 <Monster />
             </NoSsr>}
-            {loading ? <LoadingAnimation /> : null}
+            {true ? <LoadingAnimation /> : null}
         </div>
     )
 }

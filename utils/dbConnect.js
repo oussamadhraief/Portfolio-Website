2d7861845a6f253a0d226a3f830621943bed8c8f
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-08-28 01:31:22 +0100

    commit

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -8,8 +8,11 @@ async function dbConnect(){
     if(connection.isConnected){
         return
     }
+
+    const { MONGO_USER,MONGO_PASSWORD,MONGO_PATH } = process.env
+
     
-    const db = mongoose.connect(process.env.MONGO_URL, {
+    const db = mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`,{
         useUnifiedTopology: true,
         useNewUrlParser: true,
     })
